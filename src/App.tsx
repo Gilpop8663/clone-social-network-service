742df2d56b07e0c0ded14967fc7475c960d8b443
gilpop8663 <wolfye@naver.com>
2022-02-19 21:48:11 +0900

    Add - Firebase , Router 초기 세팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div>hello world</div>;
-}
+import { Home, Auth, Profile, EditProfile } from 'pages';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default App;
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Auth />} />
+        <Route path="/" element={<Profile />} />
+        <Route path="/" element={<EditProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Auth } from './Auth';
+export { default as EditProfile } from './EditProfile';
+export { default as Profile } from './Profile';
